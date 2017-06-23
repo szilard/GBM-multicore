1636f5ffe40a92bec7937a691c76d65c7fc0c26e
szilard <szilard@users.noreply.github.com>
2017-06-23 11:51:21 -0700

    doc

--- run-master.sh
+++ run-master.sh
@@ -6,4 +6,9 @@ time ./run.sh r3_8x xgboost  | tee res-r3_8x-xgboost.csv
 time ./run.sh r3_8x lightgbm | tee res-r3_8x-lightgbm.csv
 cat res-r3_8x-*.csv > res-r3_8x.csv
 
+# on r4_8x:
+time ./run.sh r4_8x h2o      | tee res-r4_8x-h2o.csv
+time ./run.sh r4_8x xgboost  | tee res-r4_8x-xgboost.csv
+time ./run.sh r4_8x lightgbm | tee res-r4_8x-lightgbm.csv
+cat res-r4_8x-*.csv > res-r4_8x.csv
 
